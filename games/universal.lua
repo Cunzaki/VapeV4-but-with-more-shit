16d92c114f95245d4c9cf3d0bb425f7af03cb750
Cunzaki <151093222+Cunzaki@users.noreply.github.com>
2026-05-10 01:27:04 -0400

    Update universal.lua

--- games/universal.lua
+++ games/universal.lua
@@ -841,10 +841,7 @@ run(function()
 							Part = Part.Value,
 							Players = Targets.Players.Enabled,
 							NPCs = Targets.NPCs.Enabled,
-<<<<<<< HEAD
-=======
-							Forcefield = Targets.Forcefield.Enabled,
->>>>>>> c383d41 (Initial fork changes)
+							Forcefield = not Targets.Forcefield or Targets.Forcefield.Enabled,
 							Wallcheck = Targets.Walls.Enabled,
 							Origin = gameCamera.CFrame.Position
 						})
@@ -1140,12 +1137,8 @@ run(function()
 			Part = targetPart,
 			Origin = origin,
 			Players = Target.Players.Enabled,
-<<<<<<< HEAD
-			NPCs = Target.NPCs.Enabled
-=======
 			NPCs = Target.NPCs.Enabled,
-			Forcefield = Target.Forcefield.Enabled
->>>>>>> c383d41 (Initial fork changes)
+			Forcefield = not Target.Forcefield or Target.Forcefield.Enabled
 		})
 
 		if ent then
@@ -1268,12 +1261,8 @@ run(function()
 							Part = 'Head',
 							Origin = (origin * fireoffset).Position,
 							Players = Target.Players.Enabled,
-<<<<<<< HEAD
-							NPCs = Target.NPCs.Enabled
-=======
 							NPCs = Target.NPCs.Enabled,
-							Forcefield = Target.Forcefield.Enabled
->>>>>>> c383d41 (Initial fork changes)
+							Forcefield = not Target.Forcefield or Target.Forcefield.Enabled
 						})
 
 						if mouse1click and (isrbxactive or iswindowactive)() then
@@ -1509,11 +1498,7 @@ run(function()
 			for _, v in entitylib.List do
 				if v.Targetable and v.Character and (Targets.Players.Enabled and v.Player or Targets.NPCs.Enabled and v.NPC) then
 					if ray.Instance:IsDescendantOf(v.Character) then
-<<<<<<< HEAD
-						return entitylib.isVulnerable(v) and v
-=======
-						return entitylib.isVulnerable(v, Targets.Forcefield.Enabled) and v
->>>>>>> c383d41 (Initial fork changes)
+						return entitylib.isVulnerable(v, not Targets.Forcefield or Targets.Forcefield.Enabled) and v
 					end
 				end
 			end
@@ -2357,10 +2342,7 @@ run(function()
 							Part = 'RootPart',
 							Players = Targets.Players.Enabled,
 							NPCs = Targets.NPCs.Enabled,
-<<<<<<< HEAD
-=======
-							Forcefield = Targets.Forcefield.Enabled,
->>>>>>> c383d41 (Initial fork changes)
+							Forcefield = not Targets.Forcefield or Targets.Forcefield.Enabled,
 							Limit = Max.Value
 						})
 	
@@ -3299,12 +3281,8 @@ run(function()
 						Wallcheck = wallcheck,
 						Part = 'RootPart',
 						Players = Targets.Players.Enabled,
-<<<<<<< HEAD
-						NPCs = Targets.NPCs.Enabled
-=======
 						NPCs = Targets.NPCs.Enabled,
-						Forcefield = Targets.Forcefield.Enabled
->>>>>>> c383d41 (Initial fork changes)
+						Forcefield = not Targets.Forcefield or Targets.Forcefield.Enabled
 					})
 	
 					if ent then
@@ -7817,8 +7795,4 @@ run(function()
 	})
 	
 end)
-<<<<<<< HEAD
 	
-=======
-	
->>>>>>> c383d41 (Initial fork changes)
