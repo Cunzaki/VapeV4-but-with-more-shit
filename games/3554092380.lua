38cb5b56660832859bd81760fff69e6af1c6c5f6
Cunzaki <151093222+Cunzaki@users.noreply.github.com>
2026-05-28 02:15:23 -0400

    Create 3554092380.lua

---
+++ games/3554092380.lua
@@ -0,0 +1,193 @@
+local run = function(func)
+    func()
+end
+local cloneref = cloneref or function(obj)
+    return obj
+end
+
+local playersService = cloneref(game:GetService("Players"))
+local inputService = cloneref(game:GetService("UserInputService"))
+local replicatedStorage = cloneref(game:GetService("ReplicatedStorage"))
+local runService = cloneref(game:GetService("RunService"))
+
+local lplr = playersService.LocalPlayer
+local vape = shared.vape
+
+-- Gun Mods (Infinite Ammo, Fire Rate, Recoil, Spread)
+run(function()
+    local GunModsModule
+    local InfiniteAmmo
+    local NoRecoil
+    local NoSpread
+    local InstantReload
+    local AutoFire
+    
+    local originalSettings = {}
+    local hookedGuns = {}
+
+    local function ApplyMods(gunModel)
+        if not gunModel then return end
+        local gunScript = gunModel:FindFirstChild("GunScript") or gunModel:FindFirstChild("RealisticV4")
+        if not gunScript then return end
+        
+        -- Find the environment or required module that holds the gun stats.
+        -- Often these games use a module in ReplicatedStorage for Weapon stats, 
+        -- but if the client has direct access to them, we can modify them via getrenv/getupvalue/etc.
+        
+        -- The decompiled scripts indicate there's a WeaponModule and a RealisticV4 script.
+        -- We will loop through the localplayer's weapons and modify the configuration.
+        task.spawn(function()
+            local env = getsenv and getsenv(gunScript)
+            if env and env.u47 then -- u47 seems to be the gun configuration table from decompiled script
+                if not originalSettings[gunModel] then
+                    originalSettings[gunModel] = {
+                        FreeAmmo = env.u47.FreeAmmo,
+                        Recoil = env.u47.Recoil,
+                        Spread = env.u47.Spread,
+                        ReloadTime = env.u47.ReloadTime,
+                        AutoFire = env.u47.AutoFire
+                    }
+                end
+                
+                if InfiniteAmmo.Enabled then
+                    env.u47.FreeAmmo = true
+                else
+                    env.u47.FreeAmmo = originalSettings[gunModel].FreeAmmo
+                end
+                
+                if NoRecoil.Enabled then
+                    env.u47.Recoil = 0
+                    env.u47.CameraRecoil = 0
+                else
+                    env.u47.Recoil = originalSettings[gunModel].Recoil
+                end
+                
+                if NoSpread.Enabled then
+                    env.u47.Spread = 0
+                    env.u47.MinSpread = 0
+                    env.u47.MaxSpread = 0
+                else
+                    env.u47.Spread = originalSettings[gunModel].Spread
+                end
+                
+                if AutoFire.Enabled then
+                    env.u47.Auto = true
+                    env.u47.FireType = "Auto"
+                end
+            end
+        end)
+    end
+
+    local function HookGuns()
+        local char = lplr.Character
+        if char then
+            for _, v in ipairs(char:GetChildren()) do
+                if v:IsA("Tool") and not hookedGuns[v] then
+                    hookedGuns[v] = true
+                    ApplyMods(v)
+                end
+            end
+        end
+        
+        local cam = workspace.CurrentCamera
+        if cam then
+            for _, v in ipairs(cam:GetChildren()) do
+                if v:IsA("Model") and not hookedGuns[v] then
+                    hookedGuns[v] = true
+                    ApplyMods(v)
+                end
+            end
+        end
+    end
+
+    GunModsModule = vape.Categories.Combat:CreateModule({
+        Name = "GunMods",
+        Function = function(callback)
+            if callback then
+                task.spawn(function()
+                    while GunModsModule.Enabled do
+                        HookGuns()
+                        task.wait(1)
+                    end
+                end)
+            else
+                for gun, orig in pairs(originalSettings) do
+                    local gunScript = gun:FindFirstChild("GunScript") or gun:FindFirstChild("RealisticV4")
+                    if gunScript then
+                        local env = getsenv and getsenv(gunScript)
+                        if env and env.u47 then
+                            env.u47.FreeAmmo = orig.FreeAmmo
+                            env.u47.Recoil = orig.Recoil
+                            env.u47.Spread = orig.Spread
+                        end
+                    end
+                end
+                table.clear(hookedGuns)
+                table.clear(originalSettings)
+            end
+        end,
+        Tooltip = "Modifies your gun's stats"
+    })
+    
+    InfiniteAmmo = GunModsModule:CreateToggle({
+        Name = "Infinite Ammo",
+        Function = function() end
+    })
+    
+    NoRecoil = GunModsModule:CreateToggle({
+        Name = "No Recoil",
+        Function = function() end
+    })
+    
+    NoSpread = GunModsModule:CreateToggle({
+        Name = "No Spread",
+        Function = function() end
+    })
+    
+    AutoFire = GunModsModule:CreateToggle({
+        Name = "Auto Fire",
+        Function = function() end
+    })
+end)
+
+-- InstaKill (Headshots)
+run(function()
+    local InstaKill
+    
+    InstaKill = vape.Categories.Combat:CreateModule({
+        Name = "InstaKill",
+        Function = function(callback)
+            if callback then
+                task.spawn(function()
+                    while InstaKill.Enabled do
+                        local char = lplr.Character
+                        local cam = workspace.CurrentCamera
+                        
+                        -- Find currently equipped gun config
+                        local currentGun = nil
+                        if cam then
+                            for _, v in ipairs(cam:GetChildren()) do
+                                if v:IsA("Model") and (v:FindFirstChild("GunScript") or v:FindFirstChild("RealisticV4")) then
+                                    currentGun = v
+                                    break
+                                end
+                            end
+                        end
+                        
+                        if currentGun then
+                            local gunScript = currentGun:FindFirstChild("GunScript") or currentGun:FindFirstChild("RealisticV4")
+                            local env = getsenv and getsenv(gunScript)
+                            if env and env.u47 then
+                                env.u47.Damage = 99999
+                                env.u47.Headshot = 99999
+                            end
+                        end
+                        
+                        task.wait(1)
+                    end
+                end)
+            end
+        end,
+        Tooltip = "Modifies gun damage to instantly kill zombies."
+    })
+end)
